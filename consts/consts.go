e511be1165aa0c4b7ee48ecb3ab18017f66e21ba
Tillo <tillo.bosshart@gmail.com>
2019-03-13 22:50:09 +0100

    time format changed

--- consts/consts.go
+++ consts/consts.go
@@ -1,5 +1,5 @@
 package consts
 
-const TimeFmtString string = "Mon Jan 2 15:04"
+const TimeFmtString string = "2006-01-02 15:04:05"
 const TimeLogFile string = "time.log"
 const TimeLockFile string = "time.lck"
