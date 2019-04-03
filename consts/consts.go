750a2afbc341333c53c988d7150b7710c4b90cf1
Tillo Bosshart <tillo.bosshart@kubeg.ch>
2019-04-03 16:25:45 +0200

    added new format for the lockfile

--- consts/consts.go
+++ consts/consts.go
@@ -1,5 +1,9 @@
 package consts
 
-const TimeFmtString string = "2006-01-02 15:04:05"
+import "time"
+
+const TimeLogFmtString string = "2006-01-02 15:04:05"
+
+const TimeFmtString string = time.RFC3339
 const TimeLogFile string = "time.log"
 const TimeLockFile string = "time.lck"
