5b71f18aef2acd82ebe84205b686a1b755c22549
Tillo <tillo.bosshart@gmail.com>
2019-04-04 16:10:58 +0200

    Help messages cleaned up

--- cmd/login.go
+++ cmd/login.go
@@ -19,16 +19,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// loginCmd represents the login command
 var loginCmd = &cobra.Command{
 	Use:   "login",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Starts time logging. If it already is logging, it stops it first",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) > 0 {
 			proj := strings.Join(args, " ")
--- cmd/root.go
+++ cmd/root.go
@@ -22,13 +22,13 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "timelogger",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A simple time logger",
+	Long: `Let's you log times for your projects, one at the time.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+timelogger login <project> starts logging
+timelogger logout stops logging
+
+a login while it's logging, stops logging first and then change the project.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
