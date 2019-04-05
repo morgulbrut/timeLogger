d57646bedf989d12f5338fce55d132d561e20534
Tillo <tillo.bosshart@gmail.com>
2019-04-05 08:46:16 +0200

    logo added

--- cmd/root.go
+++ cmd/root.go
@@ -9,15 +9,22 @@ as published by Sam Hocevar. See the LICENSE file or
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
+	"github.com/morgulbrut/color"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
+var logo = `
+ _______           __                         
+/_  __(_)_ _  ___ / /  ___  ___ ____ ____ ____
+ / / / /  ' \/ -_) /__/ _ \/ _ '/ _ '/ -_) __/
+/_/ /_/_/_/_/\__/____/\___/\_, /\_, /\__/_/   
+                          /___//___/          
+`
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -31,7 +38,19 @@ timelogger logout stops logging
 a login while it's logging, stops logging first and then change the project.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		color.HiCyan("%s", logo)
+		color.Yellow("Please enter project to login to:")
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			proj := scanner.Text()
+			if proj == "logout" {
+				Logout()
+			} else {
+				Login(scanner.Text())
+			}
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -40,44 +59,5 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.timelogger.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".timelogger" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".timelogger")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
