616f996bbccc506e2c34cbe5c769a4b6d312ae8d
Tillo <tillo.bosshart@gmail.com>
2019-05-08 00:05:26 +0200

    show projects added

---
+++ cmd/projects.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2019 morgulbrut
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the LICENSE file or
+ http://www.wtfpl.net/ for more details.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// projectsCmd represents the projects command
+var projectsCmd = &cobra.Command{
+	Use:   "projects",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("projects called")
+	},
+}
+
+func init() {
+	showCmd.AddCommand(projectsCmd)
+}
