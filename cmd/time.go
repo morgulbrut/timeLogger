950717dad03cec7fb902034b89b165f7d22d1d65
Tillo <tillo.bosshart@gmail.com>
2019-05-08 00:01:56 +0200

    show time implemented

--- cmd/time.go
+++ cmd/time.go
@@ -9,9 +9,15 @@ as published by Sam Hocevar. See the LICENSE file or
 package cmd
 
 import (
-	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
+	"github.com/morgulbrut/timeLogger/consts"
+
+	"github.com/morgulbrut/color"
 	"github.com/spf13/cobra"
+	"github.com/yunabe/easycsv"
 )
 
 // timeCmd represents the time command
@@ -19,10 +25,36 @@ var timeCmd = &cobra.Command{
 	Use:   "time",
 	Short: "Shows the time accumulated on a project",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("time called")
+		project := strings.Join(args, " ")
+		time := getTime(project)
+		color.Green("Total time for %q: %0.2f h", project, time)
 	},
 }
 
 func init() {
 	showCmd.AddCommand(timeCmd)
+}
+
+func getTime(project string) float64 {
+	color.Yellow("Showing total time for %q", project)
+	time := 0.0
+	r := easycsv.NewReaderFile(consts.TimeLogFile)
+	var entry struct {
+		Project string `index:"0"`
+		Time    string `index:"3"`
+	}
+	for r.Read(&entry) {
+		if entry.Project == project {
+			color.Yellow("%s", entry.Time)
+			h, _ := strconv.ParseFloat(strings.Split(entry.Time, ":")[0], 64)
+			m, _ := strconv.ParseFloat(strings.Split(entry.Time, ":")[1], 64)
+
+			time += h + m/60
+		}
+	}
+	if err := r.Done(); err != nil {
+		log.Fatalf("Failed to read a CSV file: %v", err)
+	}
+
+	return time
 }
