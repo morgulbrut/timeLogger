fa2983c4db1a6a63f29594f6254d4c69d2613e42
Tillo <tillo.bosshart@gmail.com>
2019-05-08 11:47:55 +0200

    show project implemented

--- cmd/projects.go
+++ cmd/projects.go
@@ -9,26 +9,49 @@ as published by Sam Hocevar. See the LICENSE file or
 package cmd
 
 import (
-	"fmt"
+	"log"
 
+	"github.com/morgulbrut/color"
+	"github.com/morgulbrut/timeLogger/consts"
 	"github.com/spf13/cobra"
+	"github.com/yunabe/easycsv"
 )
 
 // projectsCmd represents the projects command
 var projectsCmd = &cobra.Command{
 	Use:   "projects",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Lists the projects in the log",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("projects called")
+		projects := getProjects()
+		for pr := range projects {
+			color.Green(projects[pr])
+		}
 	},
 }
 
 func init() {
 	showCmd.AddCommand(projectsCmd)
+}
+
+func getProjects() []string {
+	color.Yellow("Listing projects")
+	r := easycsv.NewReaderFile(consts.TimeLogFile)
+	set := make(map[string]struct{})
+	projects := []string{}
+	var entry struct {
+		Project string `index:"0"`
+	}
+	for r.Read(&entry) {
+
+		if entry.Project != "project" {
+			set[entry.Project] = struct{}{}
+		}
+	}
+	if err := r.Done(); err != nil {
+		log.Fatalf("Failed to read a CSV file: %v", err)
+	}
+	for key := range set {
+		projects = append(projects, key)
+	}
+	return projects
 }
