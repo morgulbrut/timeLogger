2feba9ceaec3ab523c13f1c7603cdfa72fa86f71
Tillo Bosshart <tillo.bosshart@kubeg.ch>
2019-08-27 15:19:46 +0200

    fixed format

--- cmd/logout.go
+++ cmd/logout.go
@@ -5,6 +5,7 @@ terms of the Do What The Fuck You Want To Public License, Version 2,
 as published by Sam Hocevar. See the LICENSE file or
  http://www.wtfpl.net/ for more details.
 */
+
 package cmd
 
 import (
