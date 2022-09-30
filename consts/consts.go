46b29e157127904342df5b2904cb03a1519fb9d6
morgulbrut <tillo.bosshart@gmail.com>
2022-09-30 13:11:18 +0200

    change file location

--- consts/consts.go
+++ consts/consts.go
@@ -5,5 +5,5 @@ import "time"
 const TimeLogFmtString string = "2006-01-02 15:04:05"
 
 const TimeFmtString string = time.RFC3339
-const TimeLogFile string = "time.csv"
-const TimeLockFile string = "time.lck"
+const TimeLogFile string = "C:/Users/tbosshar/OneDrive - Turck Vilant Systems Oy/Documents/time.csv"
+const TimeLockFile string = "C:/Users/tbosshar/OneDrive - Turck Vilant Systems Oy/Documents/time.lck"
