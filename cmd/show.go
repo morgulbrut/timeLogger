e04e46729dc76471c14e9c043f4d461ad9026989
morgulbrut <tillo.bosshart@gmail.com>
2022-09-30 13:12:23 +0200

    Added logged in project and time to show

--- cmd/show.go
+++ cmd/show.go
@@ -9,12 +9,19 @@ as published by Sam Hocevar. See the LICENSE file or
 package cmd
 
 import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"runtime"
+	"strings"
+	"time"
 
 	"github.com/jedib0t/go-pretty/table"
+	"github.com/morgulbrut/color256"
 	"github.com/morgulbrut/timeLogger/consts"
+	"github.com/morgulbrut/timeLogger/utils"
 	"github.com/spf13/cobra"
 	"github.com/yunabe/easycsv"
 )
@@ -30,6 +37,22 @@ var showCmd = &cobra.Command{
 			Project string `index:"0"`
 			Date    string `index:"2"`
 			Time    string `index:"3"`
+		}
+		if _, err := os.Stat(consts.TimeLockFile); err == nil {
+			dat, err := ioutil.ReadFile(consts.TimeLockFile)
+			if err != nil {
+				utils.Error(err.Error())
+			}
+			log := strings.TrimRight(string(dat), "\r\n")
+			timeStrings := strings.Split(log, ";")
+			timeString := timeStrings[len(timeStrings)-1]
+			loginTime, err := time.Parse(consts.TimeFmtString, timeString)
+			logtime := time.Now()
+			duration := logtime.Sub(loginTime)
+			durString := fmt.Sprintf("%d:%02d", int(duration.Minutes()/60), int(duration.Minutes())%60)
+			color256.PrintHiOrange("Logged in to: %s for %s", timeStrings[1], durString)
+		} else {
+			color256.PrintHiMagenta("Not logged in")
 		}
 
 		t := table.NewWriter()
@@ -49,6 +72,8 @@ var showCmd = &cobra.Command{
 			}
 		}
 		t.Render()
+		color256.PrintCyan("Press any key to close")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
 	},
 }
 
